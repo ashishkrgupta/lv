e389b79c65a9751d939f0c5bc7f1586c50e8cd20
Ashish <ashishkrgupta@hotmail.com>
2020-03-13 18:46:31 +0530

    Updates

---
+++ precache-manifest.09e931726d0eb698dbd38530702607b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8ea20d7c4da45f61c473228f7f32c2",
+    "url": "/lv/index.html"
+  },
+  {
+    "revision": "93a0a2ec9e9a01a27c91",
+    "url": "/lv/static/css/main.bee37f14.chunk.css"
+  },
+  {
+    "revision": "d3c6f4731546646e72cb",
+    "url": "/lv/static/js/2.987bf471.chunk.js"
+  },
+  {
+    "revision": "3521e1838e10dc5bd0f58638f186a98e",
+    "url": "/lv/static/js/2.987bf471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a0a2ec9e9a01a27c91",
+    "url": "/lv/static/js/main.4ab73851.chunk.js"
+  },
+  {
+    "revision": "7607aa97a8b75eaad412",
+    "url": "/lv/static/js/runtime-main.ce4a8bb4.js"
+  }
+]);
